3013b13781fe8b3216adb37980feaa015d75d4ca
subhashishjungshah <subhashishjungshah@gmail.com>
2024-01-17 10:01:40 +0545

    learning tanstack mutation and query

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+
+const posts = [
+  { id: 1, title: "Post 1", content: "Content 1" },
+  { id: 2, title: "Post 2", content: "Content 2" },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const queryClient = useQueryClient();
 
+  const postQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      await wait(1000);
+      return [...posts];
+    },
+  });
+
+  const newPostMutation = useMutation({
+    mutationFn: async () => {
+      await wait(1000);
+      return posts.push({ id: 3, title: "Post 3", content: "Content 3" });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries("posts");
+    },
+  });
+
+  if (postQuery.isLoading) return <h1>Loading...</h1>;
+  if (postQuery.isError) return <h1>Error</h1>;
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{postQuery.data.map((post) => post.title)}</h1>
+      <button
+        disabled={newPostMutation.isLoading}
+        onClick={() => newPostMutation.mutate()}
+      >
+        Add new post
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+function wait(duration) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+}
+
+export default App;
